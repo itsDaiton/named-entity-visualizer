1074b49dea87d4a3a147d63cf9e6b539fbe1269b
David Poslušný <david.poslusny@email.cz>
2023-12-29 19:52:48 +0100

    refactor: baseline script

---
+++ script.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import spacy
+from spacy import displacy
+
+nlp = spacy.load('en_core_web_md')
+
+st.title("Named Entity Recognition Visualizer Application")
+st.header("Visualizer")
+
+menu = ["Item 1","Item 2"]
+choice = st.sidebar.selectbox("Menu",menu)
+
+input = """When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously. “I can tell you very senior CEOs of major American car companies would shake my hand and turn away because I wasn’t worth talking to,” said Thrun, now the co-founder and CEO of online higher education startup Udacity, in an interview with Recode earlier this week.
+A little less than a decade later, dozens of self-driving startups have cropped up while automakers around the world clamor, wallet in hand, to secure their place in the fast-moving world of fully automated transportation.
+"""
+
+input_text = st.text_area('Text to analyze:', input)
+doc = nlp(input_text)
+
+st.header('Entities found on the Wikidata')
+ent_html = displacy.render(doc, style='ent', jupyter=False)
+st.markdown(ent_html, unsafe_allow_html=True)

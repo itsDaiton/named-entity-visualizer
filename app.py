3b64afc59b2aa9a4af4f91c0daf5213cc6f140c6
David Poslušný <david.poslusny@email.cz>
2024-01-02 14:43:29 +0100

    build: new project structure

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import spacy
 from spacy import displacy
+from layout import load_main, load_sidebar
 
 nlp = spacy.load('en_core_web_md')
+nlp = spacy.load('en_core_web_sm')
 
-st.title("Named Entity Recognition Visualizer Application")
-st.header("Visualizer")
-
-menu = ["Item 1","Item 2"]
-choice = st.sidebar.selectbox("Menu",menu)
+load_sidebar()
+load_main()
 
 input = """When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously. “I can tell you very senior CEOs of major American car companies would shake my hand and turn away because I wasn’t worth talking to,” said Thrun, now the co-founder and CEO of online higher education startup Udacity, in an interview with Recode earlier this week.
 A little less than a decade later, dozens of self-driving startups have cropped up while automakers around the world clamor, wallet in hand, to secure their place in the fast-moving world of fully automated transportation.
---
+++ layout.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def load_main():
+  st.header("Visualizer")
+  st.title("Named Entity Recognition Visualizer Application")
+
+def load_sidebar():
+  st.sidebar.header('Test123')

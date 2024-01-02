fa7f1d64c306b3d078323342648738ed5bab3df0
David Poslušný <david.poslusny@email.cz>
2024-01-02 17:26:56 +0100

    refactor: model loading

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 import spacy
 from processing import process_text, render_entities, get_default_input
 
+@st.cache_resource
+def load_model(model):
+    return spacy.load(model)
+
+# Load models
+model_sm = load_model('en_core_web_sm')
+model_md = load_model('en_core_web_md')
+model_lg = load_model('en_core_web_lg')
+model_trf = load_model('en_core_web_trf')
+
 # Set default model
 if 'current_model' not in st.session_state:
     st.session_state.current_model = 'en_core_web_sm'
@@ -24,13 +34,14 @@ description = '<p style="font-size: 18px">NERV short for <span style="color: Ind
 st.sidebar.markdown(description, unsafe_allow_html=True)
 
 # Sidebar model selection
-model_options = ['en_core_web_sm', 'en_core_web_md']
-st.session_state.current_model = st.sidebar.selectbox('Select a model', model_options)
+model_options = {'en_core_web_sm': model_sm, 'en_core_web_md': model_md, 'en_core_web_lg': model_lg, 'en_core_web_trf': model_trf}
+st.session_state.current_model = st.sidebar.selectbox('Select a model', model_options.keys())
 
 # Sidebar model description
 st.sidebar.title('Model description')
-model_description = spacy.load(st.session_state.current_model).meta['description']
-model_version = spacy.load(st.session_state.current_model).meta['version']
+selected_model = model_options[st.session_state.current_model]
+model_description = selected_model.meta['description']
+model_version = selected_model.meta['version']
 model_info = f'<p style="font-size: 16px"><b>{st.session_state.current_model}:</b> <code>{model_version}</code>{model_description}</p>'
 st.sidebar.markdown(model_info, unsafe_allow_html=True)
 

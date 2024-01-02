40b133cfa89cd3243daa159826a9f8fc5217f8b1
David Poslušný <david.poslusny@email.cz>
2024-01-02 20:30:56 +0100

    feat: entity filtering

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
 import streamlit as st
 import spacy
-from processing import process_text, render_entities, get_default_input
+from processing import process_text, render_entities, get_default_input, get_entity_colors
 
+# Load models only when necessary - https://docs.streamlit.io/library/advanced-features/caching
 @st.cache_resource
 def load_model(model):
     return spacy.load(model)
 
-# Load models
+# Load English pre-trained models - https://spacy.io/models/en
 model_sm = load_model('en_core_web_sm')
 model_md = load_model('en_core_web_md')
 model_lg = load_model('en_core_web_lg')
 model_trf = load_model('en_core_web_trf')
 
-# Set default model
+# Load labels and their colors - https://spacy.io/api/language#get_pipe
+entity_labels = model_sm.get_pipe('ner').labels
+entity_colors = get_entity_colors()
+
+# Set default model to session state - https://docs.streamlit.io/library/api-reference/session-state
 if 'current_model' not in st.session_state:
     st.session_state.current_model = 'en_core_web_sm'
-
-# Input text
-input_text = st.text_area('Text to analyze', get_default_input(), height=250)
-doc = process_text(input_text, st.session_state.current_model)
-
-# Named Entities
-st.header('Entities found on the Wikidata')
-ent_html = render_entities(doc)
-st.markdown(ent_html, unsafe_allow_html=True)
+    
+# Set default entities to session state - https://docs.streamlit.io/library/api-reference/session-state
+if 'selected_options' not in st.session_state:
+    st.session_state.selected_options = entity_labels
 
 # Sidebar title
 title = '<p style="color: Indigo; font-size: 54px; font-family: Segoe UI;">NERV</p>'
@@ -45,4 +45,33 @@ model_version = selected_model.meta['version']
 model_info = f'<p style="font-size: 16px"><b>{st.session_state.current_model}:</b> <code>{model_version}</code>{model_description}</p>'
 st.sidebar.markdown(model_info, unsafe_allow_html=True)
 
+# Named entities selection
 st.sidebar.title('Named Entities')
+container = st.sidebar.container()
+all = st.sidebar.checkbox('Select all entities', value=True)
+
+# Select all entities by default, else select only the ones that are already selected
+if all:
+  st.session_state.selected_options = container.multiselect('Select entities to display', entity_labels, default=entity_labels)
+else: 
+  st.session_state.selected_options = container.multiselect('Select entities to display', entity_labels)
+
+# Set different colors for each entity
+for label, color in entity_colors.items():
+  st.sidebar.markdown(
+    f"""
+    <style>
+        span[data-baseweb="tag"]:has(span[title={label}]) {{
+            background-color: {color} !important;
+        }}
+    </style>
+    """, unsafe_allow_html=True)
+  
+# Input text to analyze
+input_text = st.text_area('Text to analyze', get_default_input(), height=250)
+doc = process_text(input_text, st.session_state.current_model)
+
+# Named Entities - https://spacy.io/usage/visualizers
+st.header('Named Entities')
+ent_html = render_entities(doc, st.session_state.selected_options)
+st.markdown(ent_html, unsafe_allow_html=True)
--- processing.py
+++ processing.py
@@ -1,15 +1,41 @@
-import streamlit as st
 import spacy
 from spacy import displacy
 
+# Load default input text
 def get_default_input():
   return """When Sebastian Thrun started working on self-driving cars at Google in 2007, few people outside of the company took him seriously. “I can tell you very senior CEOs of major American car companies would shake my hand and turn away because I wasn’t worth talking to,” said Thrun, now the co-founder and CEO of online higher education startup Udacity, in an interview with Recode earlier this week. A little less than a decade later, dozens of self-driving startups have cropped up while automakers around the world clamor, wallet in hand, to secure their place in the fast-moving world of fully automated transportation.
 """
 
+# Process input text with selected model and its NLP pipeline
 def process_text(input_text, model):
   nlp = spacy.load(model)
   doc = nlp(input_text)
   return doc
 
-def render_entities(doc):
-  return displacy.render(doc, style='ent', jupyter=False)
+# Render entities using displacy.render function - https://spacy.io/usage/visualizers
+def render_entities(doc, selected_options):
+  options = {'ents': selected_options, 'colors': get_entity_colors()}
+  return displacy.render(doc, style='ent', jupyter=False, options=options)
+
+# Dictionary for entity colors
+def get_entity_colors():
+  return {
+    'CARDINAL': 'LimeGreen',
+    'DATE': 'LightSalmon',
+    'EVENT': 'Olive',
+    'FAC': 'SlateBlue',
+    'GPE': 'Teal',
+    'LANGUAGE': 'Orange',
+    'LAW': 'FireBrick',
+    'LOC': 'CadetBlue',
+    'MONEY': 'LightSkyBlue',
+    'NORP': 'DeepPink',
+    'ORDINAL': 'DarkCyan',
+    'ORG': 'IndianRed',
+    'PERCENT': '#FF0000',
+    'PERSON': 'Peru',
+    'PRODUCT': 'DarkOrange',
+    'QUANTITY': 'RosyBrown',
+    'TIME': 'DarkSeaGreen',
+    'WORK_OF_ART': 'MidnightBlue',
+  }
